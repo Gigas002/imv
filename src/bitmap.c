aab5d0c791f9de8b1149240b40621ad16fff0235
Marcin Serwin <marcin@serwin.dev>
2025-11-27 20:23:54 +0100

    Remove unused imv_bitmap_clone function
    Signed-off-by: Marcin Serwin <marcin@serwin.dev>

--- src/bitmap.c
+++ src/bitmap.c
@@ -1,19 +1,6 @@
 #include "bitmap.h"
 
 #include <stdlib.h>
-#include <string.h>
-
-struct imv_bitmap *imv_bitmap_clone(struct imv_bitmap *bmp)
-{
-  struct imv_bitmap *copy = malloc(sizeof *copy);
-  const size_t num_bytes = 4 * bmp->width * bmp->height;
-  copy->width = bmp->width;
-  copy->height = bmp->height;
-  copy->format = bmp->format;
-  copy->data = malloc(num_bytes);
-  memcpy(copy->data, bmp->data, num_bytes);
-  return copy;
-}
 
 void imv_bitmap_free(struct imv_bitmap *bmp)
 {
